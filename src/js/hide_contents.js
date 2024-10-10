568a863d95504a6ebcb67473e466de239487ed9f
Amirassi87 <ameraabdulla@gmail.com>
2024-10-10 13:47:59 +0900

    first commit

--- src/js/hide_contents.js
+++ src/js/hide_contents.js
@@ -2,4 +2,116 @@ export function showParagraph() {
   document.getElementById('about-us__second-part').style.position = 'static'
   document.getElementById('about-us__third-part').style.position = 'static'
 }
+
+export function openMenu() {
+  document.getElementById('navbar').style.position = 'absolute'
+  document.getElementById('navbar').style.zIndex = 10
+  document.getElementById('navbar').style.top = 0
+  document.getElementById('navbar').style.left = 0
+  document.getElementById('main').style.opacity = 0.1
+
+  document.getElementById('navbar').classList.add('sidebar--transition--in')
+}
+
+export function closeMenu() {
+  document.getElementById('navbar').style.position = 'absolute'
+  document.getElementById('navbar').style.left = '-1000px'
+  document.getElementById('main').style.opacity = 1
+  document.getElementById('navbar').classList.add('sidebar--transition--out')
+}
+
+export function openFeedBack() {
+  document.getElementById('feedback').style.position = 'absolute'
+  document.getElementById('feedback').style.zIndex = 30
+  document.getElementById('feedback').style.top = 0
+  document.getElementById('feedback').style.right = 0
+
+  document.getElementById('main').style.opacity = 0.1
+  document.getElementById('navbar').style.opacity = 0.1
+
+  document.getElementById('feedback').classList.add('feedback--transition--in')
+}
+
+export function closeFeedBack() {
+  document.getElementById('feedback').style.position = 'absolute'
+  document.getElementById('feedback').style.right = '-1000px'
+  document.getElementById('main').style.opacity = 1
+  document.getElementById('navbar').style.opacity = 1
+
+  document.getElementById('feedback').classList.add('feedback--transition--out')
+}
+
+export function openOrderCall() {
+  document.getElementById('order-call').style.position = 'absolute'
+  document.getElementById('order-call').style.zIndex = 30
+  document.getElementById('order-call').style.top = 0
+  document.getElementById('order-call').style.right = 0
+
+  document.getElementById('main').style.opacity = 0.1
+  document.getElementById('navbar').style.opacity = 0.1
+
+  document
+    .getElementById('order-call')
+    .classList.add('order-call--transition--in')
+}
+
+export function closeOrderCall() {
+  document.getElementById('order-call').style.position = 'absolute'
+  document.getElementById('order-call').style.right = '-1000px'
+  document.getElementById('main').style.opacity = 1
+  document.getElementById('navbar').style.opacity = 1
+
+  document
+    .getElementById('order-call')
+    .classList.add('order-call--transition--out')
+}
+
+//check the current mouse click value and close menues if click is on blur area
+window.addEventListener('click', function (event) {
+  let menu = getComputedStyle(
+    document.getElementById('navbar')
+  ).getPropertyValue('left')
+
+  let call = getComputedStyle(
+    document.getElementById('order-call')
+  ).getPropertyValue('right')
+
+  let feedback = getComputedStyle(
+    document.getElementById('feedback')
+  ).getPropertyValue('right')
+
+  if (menu == '0px') {
+    window.closeMenu()
+  } else if (call == '0px') {
+    window.closeOrderCall()
+  } else if (feedback == '0px') {
+    window.closeFeedBack()
+  }
+})
+
+//fix the sidebar menu in desktop size windows only
+// export function changeSidebarPos(windowSize) {
+//   if (windowSize.matches) {
+//     document.getElementById('navbar').style.position = 'static'
+//   } else {
+//     document.getElementById('navbar').style.position = 'absolute'
+//   }
+// }
+
+// let windowSize = window.matchMedia('(min-width: 1024px)')
+
+// changeSidebarPos(windowSize)
+
+// windowSize.addEventListener('change', function () {
+//   changeSidebarPos(windowSize)
+// })
+
+//make the function global
 window.showParagraph = showParagraph
+window.openMenu = openMenu
+window.closeMenu = closeMenu
+window.openFeedBack = openFeedBack
+window.closeFeedBack = closeFeedBack
+window.openOrderCall = openOrderCall
+window.closeOrderCall = closeOrderCall
+// window.changeSidebarPos = changeSidebarPos
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-=======
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
->>>>>>> 8e973de105bffc901698cd61e2d454c4c22ef4b6
 
 module.exports = {
   // Entry file
   entry: [
-<<<<<<< HEAD
     path.resolve(__dirname, './src/js/index.js'),
     path.resolve(__dirname, './src/scss/style.scss')
   ],
@@ -26,19 +18,6 @@ module.exports = {
 
   // Source maps for easier debugging
   devtool: 'source-map',
-=======
-    './src/js/index.js'
-  ],
-
-  // Output file
-  output: {
-    filename: './js/bundle.js',
-    path: path.resolve(__dirname, 'dist') // Add output path
-  },
-
-  // Source maps for easier debugging
-  devtool: "source-map",
->>>>>>> 8e973de105bffc901698cd61e2d454c4c22ef4b6
 
   module: {
     rules: [
@@ -50,11 +29,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-<<<<<<< HEAD
             presets: ['@babel/preset-env']
-=======
-            presets: ['@babel/preset-env'],
->>>>>>> 8e973de105bffc901698cd61e2d454c4c22ef4b6
           }
         }
       },
@@ -66,7 +41,6 @@ module.exports = {
           MiniCssExtractPlugin.loader, // Extract css to separate file
           'css-loader', // translates CSS into CommonJS
           'postcss-loader', // parse CSS and add vendor prefixes to CSS rules
-<<<<<<< HEAD
           'sass-loader' // compiles Sass to CSS, using Node Sass by default
         ]
       },
@@ -74,10 +48,6 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader']
-=======
-          'sass-loader', // compiles Sass to CSS, using Node Sass by default
-        ],
->>>>>>> 8e973de105bffc901698cd61e2d454c4c22ef4b6
       },
 
       // Include fonts from css
@@ -96,41 +66,24 @@ module.exports = {
         generator: {
           filename: 'static/[name][ext]'
         }
-<<<<<<< HEAD
       }
     ]
-=======
-      },
-    ],
->>>>>>> 8e973de105bffc901698cd61e2d454c4c22ef4b6
   },
   plugins: [
     // Include html file, styles and scripts will be automatically injected
     new HtmlWebpackPlugin({
-<<<<<<< HEAD
       title: 'CBS FINAL',
-=======
-      title: 'Webpack 5 Starter',
->>>>>>> 8e973de105bffc901698cd61e2d454c4c22ef4b6
       template: './src/index.html',
       inject: true,
       minify: {
         removeComments: true,
-<<<<<<< HEAD
         collapseWhitespace: false
-=======
-        collapseWhitespace: false,
->>>>>>> 8e973de105bffc901698cd61e2d454c4c22ef4b6
       }
     }),
 
     // Extract styles to a separate file
     new MiniCssExtractPlugin({
-<<<<<<< HEAD
       filename: 'style.css'
-=======
-      filename: 'style.css',
->>>>>>> 8e973de105bffc901698cd61e2d454c4c22ef4b6
     }),
 
     // Copy images
@@ -138,13 +91,8 @@ module.exports = {
       patterns: [
         {
           from: './src/img',
-<<<<<<< HEAD
           to: 'img'
         }
-=======
-          to: 'img',
-        },
->>>>>>> 8e973de105bffc901698cd61e2d454c4c22ef4b6
       ]
     })
   ],
@@ -154,19 +102,10 @@ module.exports = {
     port: 9000,
     hot: true, // Enable Hot Module Replacement
     watchFiles: {
-<<<<<<< HEAD
       paths: ['src/**/*'] // Watch for changes in source files
-=======
-      paths: ['src/**/*'], // Watch for changes in source files
->>>>>>> 8e973de105bffc901698cd61e2d454c4c22ef4b6
     },
     client: {
       overlay: true // Show errors and warnings in the browser
     }
-<<<<<<< HEAD
   }
 }
-=======
-  },
-};
->>>>>>> 8e973de105bffc901698cd61e2d454c4c22ef4b6
